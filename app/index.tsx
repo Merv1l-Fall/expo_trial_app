ebf33e9b75b4854e31d3358a0a49ef894edaa5d0
Vilmer Fall <vilmer.fall@ecologforestry.com>
2026-04-01 11:42:47 +0200

    basics of useColorScheme hook

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,42 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, useColorScheme } from "react-native";
 import { Logo } from "../assets/logo/Logo";
 import { Link } from "expo-router";
+import { Colors } from "../constants/colors";
 
 
 const Home = () => {
+    const colorScheme = useColorScheme();
+    const theme = Colors[colorScheme === "dark" ? "dark" : "light"];
+
+    const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: theme.background,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: theme.title,
+    },
+    subTitle: {
+        marginTop: 10,
+        marginBottom: 30,
+        color: theme.text,
+    },
+    logo: {
+        marginVertical: 20,
+    },
+    linkButton: {
+        marginTop: 20,       
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: Colors.primary,
+        borderRadius: 5,
+    }
+});
+
     return (
         <View style={styles.container}>
             
@@ -22,30 +55,6 @@ const Home = () => {
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: "bold",
-    },
-    subTitle: {
-        marginTop: 10,
-        marginBottom: 30,
-    },
-    logo: {
-        marginVertical: 20,
-    },
-    linkButton: {
-        marginTop: 20,       
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        backgroundColor: "#007AFF",
-        borderRadius: 5,
-    }
-});
+
 
 export default Home;
---
+++ constants/colors.ts
@@ -0,0 +1,25 @@
+export const Colors = {
+    primary: "#8b5cf6",
+    warning: "#cc475a",
+
+    dark: {
+        title: "#fff",
+        text: "#d4d4d4",
+        background: "#252231",
+        navBackground: "#211e2b",
+        iconColor: "#9591a5",
+        iconColorFocused: "#fff",
+        uiBackground: "#2f2b3d",
+
+    },
+    light: {
+        title: "#625f72",
+        text: "#201e2b",
+        background: "#e0dfe0",
+        navBackground: "#e8e7ef",
+        iconColor: "#686477",
+        iconColorFocused: "#201e2b",
+        uiBackground: "#d6d5e1",
+    }
+
+}
